0a3073e7c888c72d4591c8db317534133f8cb56f
sparrowShadow <romanenko.aleksandre@gmail.com>
2024-05-15 15:27:02 +0200

    LaunchScreenWith animation

---
+++ ChineseCharacters/ViewControllers/LauchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LauchViewController.swift
+//  ChineseCharacters
+//
+//  Created by mac on 15.05.2024.
+//
+
+import UIKit
+
+class LauchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
